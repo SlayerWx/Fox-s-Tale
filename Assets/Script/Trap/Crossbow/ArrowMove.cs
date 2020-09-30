09cf5dd198e976e86ddf440d96bd7d09066f2fcf
Maxi Ruffo <maxi_cs_95@hotmail.com>
2020-09-30 10:11:29 -0300

    torreta de flechas #77 y vuelta a anterior modalidad de mapeado

---
+++ Assets/Script/Trap/Crossbow/ArrowMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowMove : MonoBehaviour
+{
+    bool readyToMove;
+    [SerializeField] float timerLife = 0.0f;
+    [SerializeField] float speed = 0.0f;
+    Rigidbody2D myRigid;
+    bool toDead;
+    // asAS
+    void Start()
+    {
+        myRigid = GetComponent<Rigidbody2D>();
+        readyToMove = false;
+        toDead = false;
+    }
+
+    // asAS
+    void FixedUpdate()
+    {
+        if(readyToMove)
+        {
+            myRigid.velocity = (transform.TransformDirection(Vector2.right) * speed) * Time.deltaTime;
+        }
+    }
+    public void Ready()
+    {
+        readyToMove = true;
+        if(!toDead)
+        {
+            toDead = true;
+            Destroy(transform.gameObject, timerLife);
+        }
+    }
+}
---
+++ Assets/Script/Trap/Crossbow/CrossbowAnimation.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrossbowAnimation : MonoBehaviour
+{
+    [SerializeField] private Sprite[] anim = null;
+    SpriteRenderer myRender;
+    GameObject arrow = null;
+    ArrowMove moveArrow = null;
+    [SerializeField] GameObject prefArrow = null;
+    [SerializeField] float correctionArrowPosY = 0.0f;
+    [SerializeField] float timeXFrame = 0.0f;
+    [SerializeField] float interval = 0.0f;
+    [SerializeField] Vector3[] arrowPosAnim = null;
+    bool isOnAnim;
+    // asAS
+    void Start()
+    {
+        myRender = GetComponentInChildren<SpriteRenderer>();
+        isOnAnim = false;
+    }
+
+    // asAS
+    void Update()
+    {
+        ShotAnim();
+    }
+    void ShotAnim()
+    {
+        if (!isOnAnim)
+        {
+            StartCoroutine(FrameSprite());
+        }
+    }
+    void SummonArrow()
+    {
+        arrow = Instantiate(prefArrow, transform.position, transform.rotation, transform);
+        arrow.transform.position = new Vector3(arrow.transform.position.x,
+                                            arrow.transform.position.y + correctionArrowPosY,
+                                            arrow.transform.position.z);
+        moveArrow = arrow.GetComponent<ArrowMove>();
+    }
+    IEnumerator FrameSprite()
+    {
+        isOnAnim = true;
+        SummonArrow();
+        for (int i = 0; i < anim.Length;i++)
+        {
+            myRender.sprite = anim[i];
+            if (i < arrowPosAnim.Length) arrow.transform.localPosition = arrowPosAnim[i];
+            else moveArrow.Ready();
+            yield return new WaitForSeconds(timeXFrame);
+        }
+       
+        yield return new WaitForSeconds(interval);
+        isOnAnim = false;
+    }
+}
