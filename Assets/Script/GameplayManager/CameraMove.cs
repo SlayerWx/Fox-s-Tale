dea5e98b934598b6b390408668fa99c999157fa3
Maxi Ruffo <maxi_cs_95@hotmail.com>
2020-10-07 15:31:03 -0300

    cambio en el movimiento de la camara

--- Assets/Script/GameplayManager/CameraMove.cs
+++ Assets/Script/GameplayManager/CameraMove.cs
@@ -5,51 +5,16 @@ using UnityEngine;
 public class CameraMove : MonoBehaviour //asAS
 {
     [SerializeField] Transform myCm = null;
-    private bool cmMoving = false;
-    Vector3 newPos = new Vector3();
-    float speed = 0;
-    float timer = 0;
-    const float timerMax = 1.0f;
-    const float zerof = 0.0f;
-    Vector3 startPos = new Vector3();
-    bool playerInPosition = false;
+    [SerializeField] Transform Player = null;
+    const float zerofOne = 0.1f;
+    float myCmZ = 0;
     void OnEnable()
     {
-        PlayerMove.PlayerGoingFoward +=ResetCamPosition;
-        playerInPosition = false;
+        myCmZ = myCm.position.z;
     }
-    void OnDisable()
+    void FixedUpdate()
     {
-        PlayerMove.PlayerGoingFoward -=ResetCamPosition;
-    }
-    void ResetCamPosition(Vector3 NewPos,float Speed)
-    {
-        if (playerInPosition)
-        {
-            startPos = myCm.position;
-            newPos = new Vector3(NewPos.x,NewPos.y ,myCm.position.z);
-            speed = Speed;
-            if (cmMoving)
-            {
-                StopCoroutine(MovingCm());
-            }
-            StartCoroutine(MovingCm());
-        }
-        else if(NewPos.y >= myCm.position.y)
-        {
-            playerInPosition = true;
-        }
-    }
-    IEnumerator MovingCm()
-    {
-        cmMoving = true;
-        timer = zerof;
-        while (timer < timerMax)
-        {
-           myCm.position = Vector3.Lerp(startPos, newPos, timer);
-            timer += Time.deltaTime * speed;
-            yield return null;
-        }
-        cmMoving = false;
+        myCm.position = Vector2.Lerp(myCm.position,Player.position, zerofOne);
+        myCm.position = new Vector3(myCm.position.x,myCm.position.y,myCmZ);
     }
 }
