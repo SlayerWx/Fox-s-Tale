3cd0fcd1e5f21c79efecfa462e3b5201a35b08ce
Maxi Ruffo <maxi_cs_95@hotmail.com>
2020-10-21 05:44:56 -0300

    water parallax

---
+++ Assets/Script/Menu/Parallax.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField] RectTransform[] layers = null;
+    [SerializeField] float[] speed = null;
+    const int two = 2;
+    const int lessTwo = -2;
+    void FixedUpdate()
+    {
+
+        for (int i = 0; i < layers.Length; i++)
+        {
+            layers[i].anchoredPosition = new Vector2(layers[i].anchoredPosition.x + (speed[(int)(i / two)] * Time.deltaTime),
+                layers[i].anchoredPosition.y);
+            if(layers[i].anchoredPosition.x - layers[i].rect.width/two  > layers[i].rect.width/two)
+            {
+                layers[i].anchoredPosition = new Vector2(layers[i].anchoredPosition.x - (layers[i].rect.width * two),layers[i].anchoredPosition.y);
+            }
+        }
+    }
+}
