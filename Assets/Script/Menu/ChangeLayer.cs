3d873b27bb502f16df9e4613c46b7fcf32cd664f
Maxi Ruffo <maxi_cs_95@hotmail.com>
2020-11-25 22:50:43 -0300

    about

---
+++ Assets/Script/Menu/ChangeLayer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeLayer : MonoBehaviour
+{
+    public void ChangeThisLayer()
+    {
+        transform.gameObject.SetActive(!transform.gameObject.activeSelf);
+    }
+}
