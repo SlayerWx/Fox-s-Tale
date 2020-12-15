c1b91fcf1ab78398f476b456bdd541871b20f2d0
Maxi Ruffo <maxi_cs_95@hotmail.com>
2020-12-15 09:48:26 -0300

    añadidas opciones en el menu principal

--- Assets/Script/Menu/PauseVolume.cs
+++ Assets/Script/Menu/PauseVolume.cs
@@ -6,12 +6,21 @@ public class PauseVolume : MonoBehaviour
 {
     [SerializeField]Slider mySound = null;
     [SerializeField]Slider mySfx = null;
+    public static float actualValueSfx = 0.5f;
+    public static float actualValueMusic = 0.5f;
+    private void Start()
+    {
+        mySfx.value = actualValueSfx;
+        mySound.value = actualValueMusic;
+    }
     public void ChangeVolumeSfx()
     {
         AkSoundEngine.SetRTPCValue("sfxVolume", mySfx.value * 100);
+        actualValueSfx = mySfx.value;
     }
     public void ChangeVolumeMusic()
     {
         AkSoundEngine.SetRTPCValue("musicVolume", mySound.value * 100);
+        actualValueMusic = mySound.value;
     }
 }
